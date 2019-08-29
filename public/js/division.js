6618ea868aceb7f236d55f9cc42414ba5e7d9024
CarlosReRi <52678400+CarlosReRi@users.noreply.github.com>
2019-08-28 20:36:04 -0500

    Se agrego archivo footer

--- public/js/division.js
+++ public/js/division.js
@@ -3,6 +3,8 @@
 		'overflow': 'hidden',
 		'height': '40%'
 		
-		});
+	});
+
 		$(".heade").load("./header.html");
 		$(".contenido").load("./contenido.html");
+		$(".footer").load("./footer.html");
